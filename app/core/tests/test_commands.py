5fce06d19d6bff82eba810c91d73d08b7fe485cc
Francisco <fdmiran@gmail.com>
2022-10-05 22:18:50 -0500

    added unite test for waiting for db delay

--- app/core/tests/test_commands.py
+++ app/core/tests/test_commands.py
@@ -20,3 +20,11 @@ class CommandTests(SimpleTestCase):
         call_command("wait_for_db")
 
         patched_check.assert_called_once_with(database=["default"])
+
+    @patch("time.sleep")
+    def test_wait_for_db_delay(self, patched_sleep, patched_check):
+        patched_check.side_effect = [Psycopg2Error] * 2[OperationalError] * 3 + [True]
+
+        call_command("wait_for_db")
+
+        patched_check.assert_called_with(database=["default"])
