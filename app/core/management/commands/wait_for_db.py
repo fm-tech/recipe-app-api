25d31e68a0dec28fc67a326a2c32713989ef6cfd
Francisco <fdmiran@gmail.com>
2022-10-06 21:44:05 -0500

    updated check command for databases

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,7 +1,8 @@
 """
 Djago command to wait for the database to be available.
 """
-
+import time
+from psycopg2 import OperationalError as PsyCoopg2Error
 from django.core.management.base import BaseCommand
 
 
@@ -9,4 +10,8 @@ class Command(BaseCommand):
     """Django command to wait for databse"""
 
     def handle(self, *args, **options):
-        pass
+        self.stdout.write("waiting for database...")
+        db_up = False
+        while db_up is False:
+            try: 
+                self.check(databases=['default'])
