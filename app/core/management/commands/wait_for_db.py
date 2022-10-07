c53ad4067b4d408325a6494ba5f143b5c292061c
Francisco <fdmiran@gmail.com>
2022-10-06 21:46:20 -0500

    added db check logic

--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -1,6 +1,7 @@
 """
 Djago command to wait for the database to be available.
 """
+from sqlite3 import OperationalError
 import time
 from psycopg2 import OperationalError as PsyCoopg2Error
 from django.core.management.base import BaseCommand
@@ -13,5 +14,11 @@ class Command(BaseCommand):
         self.stdout.write("waiting for database...")
         db_up = False
         while db_up is False:
-            try: 
-                self.check(databases=['default'])
+            try:
+                self.check(databases=["default"])
+                db_up = True
+            except (PsyCoopg2Error, OperationalError):
+                self.stdout.write("Database unavailable, waiting 1 second...")
+                time.sleep(1)
+
+        self.stdout.write(self.style.SUCCESS("Database is available"))
