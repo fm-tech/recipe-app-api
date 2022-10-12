b620ab3bb3476ed2ded490156740b746e4e3b186
Francisco <fdmiran@gmail.com>
2022-10-11 22:56:54 -0500

    made migrations and added Recipie to admin

--- app/books/models.py
+++ app/books/models.py
@@ -1,10 +1,12 @@
 from django.db import models
+from django.forms import JSONField
 
 # Create your models here.
 class Recipie(models.Model):
     name = models.CharField(max_length=40)
     description = models.CharField(max_length=50)
     active = models.BooleanField(default=True)
+    document = models.JSONField()
 
     def __str__(self):
         return self.name
